56e2dff986a557ac781c8719686b039d778665d3
Jonathan Ogata <ogata1991@gmail.com>
2022-12-24 16:47:03 -0800

    update treesitter

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "bash", "help", "c", "r", "python", "rust" },
+  ensure_installed = { "bash", "help", "c", "markdown", "r", "python", "rust" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
